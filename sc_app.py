4dbaff2b2b9d6f4f2d23803497d0facce30fe5df
antoine.lizee <antoine.lizee@gmail.com>
2018-01-27 20:10:47 +0100

    enh: v0

--- sc_app.py
+++ sc_app.py
@@ -1,8 +1,9 @@
 import json
+from pprint import pprint
 from time import sleep
 
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 print("!! Initializing app.. !!")
 app = Flask(__name__)
@@ -16,13 +17,24 @@ base_url = "https://api.sportradar.us/soccer-xt3/eu/en/%s.json?api_key={}".forma
 # pprint([(t['name'], t['id']) for t in tournaments_data['tournaments']])
 
 uefa_cl_data = json.loads(requests.get(base_url % "tournaments/sr:tournament:7/info").text)
-sleep(0.5)
+sleep(1)
 PL_data = json.loads(requests.get(base_url % "tournaments/sr:tournament:17/info").text)
-sleep(0.5)
+sleep(1)
 L1_data = json.loads(requests.get(base_url % "tournaments/sr:tournament:34/info").text)
+
+teams = uefa_cl_data['groups'][8]['teams'] + PL_data['groups'][0]['teams'] + L1_data['groups'][0]['teams']
 
 
 @app.route("/")
 def index():
-    teams = uefa_cl_data['groups'][8]['teams'] + PL_data['groups'][0]['teams'] + L1_data['groups'][0]['teams']
     return render_template("index.html", teams=teams)
+
+
+@app.route("/get-schedule", methods=['POST'])
+def get_schedule():
+    print(request.form)
+    pprint(request.form)
+    selected_team_values = request.form.getlist('selected_teams')
+    selected_teams = [t for t in teams if t['id'] in selected_team_values]
+
+    return render_template("index.html", teams=teams, selected_team_values=selected_team_values, selected_teams=selected_teams)
